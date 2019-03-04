7c8f3a9ce4fd640a0dfd149007dbd1a14399736a
azizdz12300 <44880483+azizdz12300@users.noreply.github.com>
2019-03-04 16:41:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2624,8 +2624,8 @@ message.channel.send(`** ${men.username}, :credit_card: balance` + " is `" + `${
 }
  
 if(message.content.startsWith(prefix + "daily")) {
-  if(profile[message.author.id].lastDaily != moment().format('day')) {
-    profile[message.author.id].lastDaily = moment().format('day')
+  if(profile[message.author.id].lastDaily != moment().format('24h')) {
+    profile[message.author.id].lastDaily = moment().format('24h')
     profile[message.author.id].credits += 200
      message.channel.send(`**${message.author.username} you collect your \`200\` :dollar: daily pounds**`)
 } else {
